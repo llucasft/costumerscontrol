bf62da5faa632020e66f377ffe9641ac835e2bc2
Lucas Ferreira <lucasfteo@gmail.com>
2022-10-31 07:00:52 -0300

    New home page manager
    Added the welcome manager activity to the manager to go when he logs in

---
+++ app/src/main/java/com/example/costumers_control/activity/ManagerHomeActivity.kt
@@ -0,0 +1,16 @@
+package com.example.costumers_control.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.costumers_control.R
+import com.example.costumers_control.databinding.ActivityManagerHomeBinding
+
+class ManagerHomeActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityManagerHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityManagerHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
