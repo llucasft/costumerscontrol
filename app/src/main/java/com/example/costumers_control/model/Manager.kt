fe908e42d87083f170a127e2a22ab58ecab68aeb
Lucas Ferreira <lucasfteo@gmail.com>
2022-10-31 06:37:07 -0300

    Created model Manager

---
+++ app/src/main/java/com/example/costumers_control/model/Manager.kt
@@ -0,0 +1,9 @@
+package com.example.costumers_control.model
+
+data class Manager (
+            val id: Long,
+            val name: String,
+            val cpf: String,
+            val userName: String,
+            val password: String
+        )
