3bc9c04d87583cb2a544129f25a4651861485f1c
Lucas Ferreira <lucasfteo@gmail.com>
2022-11-01 18:00:18 -0300

    Insert Manager
    Created function in dao to add a new Manager to db

--- app/src/main/java/com/example/costumers_control/database/dao/ManagerDao.kt
+++ app/src/main/java/com/example/costumers_control/database/dao/ManagerDao.kt
@@ -1,7 +1,12 @@
 package com.example.costumers_control.database.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import com.example.costumers_control.model.Manager
 
 @Dao
 interface ManagerDao {
+
+    @Insert
+    fun addNewManager(vararg manager: Manager)
 }
