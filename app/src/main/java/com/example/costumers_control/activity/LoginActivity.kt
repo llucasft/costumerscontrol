a8fff3a238b1891f4466824fa952d53ce864ccf8
Lucas Ferreira <lucasfteo@gmail.com>
2022-11-02 16:27:10 -0300

    Login Authentication
    Added the click listener to login button in the home page and the code to validate if the information filled in the fields are correct to match a registered manager

--- app/src/main/java/com/example/costumers_control/activity/LoginActivity.kt
+++ app/src/main/java/com/example/costumers_control/activity/LoginActivity.kt
@@ -3,15 +3,44 @@ package com.example.costumers_control.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.costumers_control.database.AppDatabase
 import com.example.costumers_control.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var binding:ActivityLoginBinding
+
+    private val managerDao by lazy {
+        val db = AppDatabase.instance(this)
+        db.managerDao()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        with(binding){
+            btnLogin.setOnClickListener {
+                val cpf = etRegisterUserLoginCpf.text.toString()
+                val password = etRegisterUserPassword.text.toString()
+                if (cpf.equals("") or cpf.isBlank() or password.equals("") or password.isBlank()){
+                    Toast.makeText(this@LoginActivity, "Need to fill information to login", Toast.LENGTH_LONG).show()
+                } else {
+                    val managerLogin = managerDao.managerAuthentication(cpf, password)
+                    if (managerLogin?.cpf !=null){
+                        if (managerLogin.cpf == cpf && managerLogin.password == password){
+                            val intent = Intent(this@LoginActivity, ManagerHomeActivity::class.java)
+                            intent.putExtra("Name", managerLogin.name)
+                            startActivity(intent)
+                            finish()
+                        }
+                    } else {
+                        Toast.makeText(this@LoginActivity, "Authentication failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
         
         binding.tvRegister.setOnClickListener {
             val intent = Intent(this, RegisterFormActivity::class.java)
--- app/src/main/java/com/example/costumers_control/database/dao/ManagerDao.kt
+++ app/src/main/java/com/example/costumers_control/database/dao/ManagerDao.kt
@@ -2,6 +2,7 @@ package com.example.costumers_control.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.costumers_control.model.Manager
 
 @Dao
@@ -9,4 +10,7 @@ interface ManagerDao {
 
     @Insert
     fun addNewManager(vararg manager: Manager)
+
+    @Query("SELECT * FROM Manager WHERE cpf = :cpf AND password = :password")
+    fun managerAuthentication(cpf: String, password: String): Manager?
 }
